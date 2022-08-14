f54ecf8b61eb42ba68dd4986c9899e6307a1ebcd
BuildTools <unconfigured@null.spigotmc.org>
2022-08-14 10:46:11 +0800

    fix collections.abc.Iterable issue for python 3.8+

--- mcpi_e/util.py
+++ mcpi_e/util.py
@@ -1,8 +1,11 @@
-import collections
+try:
+    from collections.abc import Iterable  # noqa
+except ImportError:
+    from collections import Iterable  # noqa
 
 def flatten(l):
     for e in l:
-        if isinstance(e, collections.abc.Iterable) and not isinstance(e, str):
+        if isinstance(e, Iterable) and not isinstance(e, str):
             for ee in flatten(e): yield ee
         else: yield e
 
