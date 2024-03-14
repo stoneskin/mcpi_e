d9730ce13abb972207ab815a94f065813f946760
caa <caa.timmy@gmail.com>
2024-03-14 10:31:14 +0300

    rconn delay added

--- mcpi_e/rconnection.py
+++ mcpi_e/rconnection.py
@@ -31,6 +31,7 @@ class Rconnection:
         which is mildly distressing as it can't encode all of Unicode.
         """
         print(f"-> sendReceive RCON: {command}")
+        time.sleep(3)
         result = self.rconn.command(command)
         print(f"<- sendReceive RCON: {result}")
         return result
